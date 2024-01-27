cc155ad3da74036e3a831ee694d0a1ae89ce42f2
Franco Tejo <francotejogmail.com>
2024-01-26 22:31:32 -0300

    se crean archivos iniciales del propyecto y se define matriz del tablero

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+import Board from './components/Board/Board';
+
+const App = () => {
+
+const [turno, setTurno]= useState ('x');
+const [squares, setSquares] = useState (Array(9).fill(null));
+const [score, setScore] = useState ({
+  x:0,
+  o:0,
+});
+
+  
+  return (
+    <div className="container">
+      <Board squares={squares}/>
+    </div>
+  );
+}
+
+export default App;
